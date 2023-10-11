11d8d7dd4d0e7c7666a35d18db79495c4cbef13d
xiaoyang <2533037186@qq.com>
2023-10-11 11:06:43 +0800

    vue-i18n再修复，解决pinia初始化无数据问题

--- chen1.6/src/language/index.js
+++ chen1.6/src/language/index.js
@@ -5,8 +5,13 @@ import en from "./config/en";
 import enLocale from "element-plus/lib/locale/lang/en";
 import zhLocale from "element-plus/lib/locale/lang/zh-CN";
 
+let language
 //使用的是sessionStroage中存储的的pinia(需转化js对象)，默认是中文简体cn
-let language = JSON.parse(window.sessionStorage.getItem("useCommon")).configs.locale
+if(JSON.parse(window.sessionStorage.getItem("useCommon"))) {
+	language = JSON.parse(window.sessionStorage.getItem("useCommon")).configs.locale
+} else {
+	language = 'cn'
+}
 const i18n = createI18n({
 	locale: language ? 'cn' : 'en', //将i18n的locale设置为你需要的语言
 	messages: {
